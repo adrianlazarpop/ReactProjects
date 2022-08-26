50ebf5fc16a03e316c92592be9e8db94d2075c38
adrianlazarpop <adrian.lazar.pop@gmail.com>
2022-08-26 18:16:56 +0300

    Rolodex-monsters first steps

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,50 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda',
+          id: '12e12341e'
+        },
+        {
+          name: 'Frank',
+          id: '12e123cas'
+        },
+        {
+          name: 'Jacky',
+          id: '1243e1r2'
+        },
+        {
+          name: 'Adrian',
+          id: '221e133q1'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+
 }
 
 export default App;
